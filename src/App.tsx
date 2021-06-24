af622f3e279b99e15703365baa441dd15a73873f
Mathew Ng <mathew.ng@iress.com>
2021-06-24 22:58:01 +1000

    data shouldn't be passed in should be sourced from a service call

--- src/App.tsx
+++ src/App.tsx
@@ -3,55 +3,9 @@ import MicroFrontend from "./MicroFrontend";
 import "./index.scss";
 
 const App = () => {
-const columns = [
-    {
-      Header: 'Name',
-      columns: [
-        {
-          Header: 'First Name',
-          accessor: 'firstName',
-        },
-        {
-          Header: 'Last Name',
-          accessor: 'lastName',
-        },
-      ],
-    },
-    {
-      Header: 'Info',
-      columns: [
-        {
-          Header: 'Age',
-          accessor: 'age',
-        },
-        {
-          Header: 'Visits',
-          accessor: 'visits',
-        },
-        {
-          Header: 'Status',
-          accessor: 'status',
-        },
-        {
-          Header: 'Profile Progress',
-          accessor: 'progress',
-        },
-      ],
-    }
-]
-
-const data = [{
-  firstName: "Sam",
-  lastName: "Face",
-  age: 30,
-  visits: 5,
-  progress: 5,
-  status:'relationship'
-}]
-
-return (
-  <MicroFrontend widgetName = {"Person"} columns={columns} data={data}/>
-)
+  return (
+    <MicroFrontend />
+  )
 }
 
 export default App;
--- src/MicroFrontend.tsx
+++ src/MicroFrontend.tsx
@@ -4,57 +4,58 @@ import Header from "./Header";
 import Table from "./DataTable";
 import "./index.scss";
 
-function MicroFrontend ({ widgetName, columns, data }) {
-    const defaultColumns =  [
+function MicroFrontend () {
+  const widgetName = "Order Details Widget";
+  const columns =  [
+    {
+      Header: 'Account',
+      columns: [
         {
-          Header: 'Account',
-          columns: [
-            {
-              Header: 'Account Name',
-              accessor: 'accountName',
-            }
-          ],
-        },
-        {
-          Header: 'Order Details',
-          columns: [
-            {
-              Header: 'Created',
-              accessor: 'created',
-            },
-            {
-              Header: 'Order Number',
-              accessor: 'orderNum',
-            },
-            {
-              Header: 'Status',
-              accessor: 'status',
-            },
-          ],
-        },
-      ]
-    
-    const defaultData = [{
-      accountName: "testing@iress",
-      created: "2021-06-11 10:25:02",
-      orderNum: 123456,
-      status:'created'
+          Header: 'Account Name',
+          accessor: 'accountName',
+        }
+      ],
     },
     {
-        accountName: "testing@iress",
-        created: "2021-06-22 08:15:02",
-        orderNum: 1000122,
-        status:'fully filled'
-    }]
+      Header: 'Order Details',
+      columns: [
+        {
+          Header: 'Created',
+          accessor: 'created',
+        },
+        {
+          Header: 'Order Number',
+          accessor: 'orderNum',
+        },
+        {
+          Header: 'Status',
+          accessor: 'status',
+        },
+      ],
+    },
+  ];
+    
+  const data = [{
+    accountName: "testing@iress",
+    created: "2021-06-11 10:25:02",
+    orderNum: 123456,
+    status:'created'
+  },
+  {
+    accountName: "testing@iress",
+    created: "2021-06-22 08:15:02",
+    orderNum: 1000122,
+    status:'fully filled'
+  }];
 
-    return (
+  return (
     <Styles>
-        <div>
-        <Header widgetName={widgetName || "Order Details Widget"}/>
+      <div>
+        <Header widgetName={widgetName} />
       </div>
-        <Table columns={columns || defaultColumns} data={data || defaultData} />
-      </Styles>
-    )
+      <Table columns={columns} data={data} />
+    </Styles>
+  );
 }
 
 export default MicroFrontend;
