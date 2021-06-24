02590702ff100eff83969c1fd151290b68ebf1f8
peishan-li <peishan.li@iress.com>
2021-06-24 10:58:38 +1000

    add a table

---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => (
+  <div
+    style={{
+      padding: "1em",
+      margin: "1em",
+      background: "blue",
+      color: "white",
+      fontWeight: "bold",
+    }}
+  >
+    Header 1.0
+  </div>
+);
