9a0339a0b1759e8747322a3e9c5838db22cb25ad
peishan-li <peishan.li@iress.com>
2021-06-24 16:01:29 +1000

    add default props value

--- src/MicroFrontend.tsx
+++ src/MicroFrontend.tsx
@@ -5,12 +5,48 @@ import Table from "./DataTable";
 import "./index.scss";
 
 function MicroFrontend ({ widgetName, columns, data }) {
+    const defaultColumns =  [
+        {
+          Header: 'Account',
+          columns: [
+            {
+              Header: 'Account Name',
+              accessor: 'accountName',
+            }
+          ],
+        },
+        {
+          Header: 'Order Details',
+          columns: [
+            {
+              Header: 'Created',
+              accessor: 'created',
+            },
+            {
+              Header: 'Order Number',
+              accessor: 'orderNum',
+            },
+            {
+              Header: 'Status',
+              accessor: 'status',
+            },
+          ],
+        },
+      ]
+    
+    const defaultData = [{
+      accountName: "testing@iress",
+      created: "2021-06-11 10:25:02",
+      orderNum: 123456,
+      status:'created'
+    }]
+
     return (
     <Styles>
         <div>
-        <Header widgetName={widgetName}/>
+        <Header widgetName={widgetName || "Order Details Widget"}/>
       </div>
-        <Table columns={columns} data={data} />
+        <Table columns={columns || defaultColumns} data={data || defaultData} />
       </Styles>
     )
 }
