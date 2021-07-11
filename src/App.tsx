010a38383a64c8f7f8d7965beb1a0a44542279ff
Mathew Ng <mathew.ng@iress.com>
2021-07-12 09:42:20 +1000

    broadcast payload

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import MicroFrontend from "./MicroFrontend";
 import "./index.scss";
 
+const broadcastPayload = {};
+
 const App = () => {
   return (
-    <MicroFrontend />
+    <MicroFrontend locale="en-au" broadcastPayload={broadcastPayload}/>
   )
 }
 
--- src/MicroFrontend.tsx
+++ src/MicroFrontend.tsx
@@ -1,60 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, FunctionComponent } from 'react';
 import Styles from './Styles';
 import Header from "./Header";
 import Table from "./DataTable";
 import "./index.scss";
 
-function MicroFrontend () {
-  const widgetName = "Order Details Widget";
-  const columns =  [
-    {
-      Header: 'Account',
-      columns: [
-        {
-          Header: 'Account Name',
-          accessor: 'accountName',
-        }
-      ],
-    },
-    {
-      Header: 'Order Details',
-      columns: [
-        {
-          Header: 'Created',
-          accessor: 'created',
-        },
-        {
-          Header: 'Order Number',
-          accessor: 'orderNum',
-        },
-        {
-          Header: 'Status',
-          accessor: 'status',
-        },
-      ],
-    },
-  ];
-    
-  const data = [{
-    accountName: "testing@iress",
-    created: "2021-06-11 10:25:02",
-    orderNum: 123456,
-    status:'created'
-  },
-  {
-    accountName: "testing@iress",
-    created: "2021-06-22 08:15:02",
-    orderNum: 1000122,
-    status:'fully filled'
-  }];
+export interface IMicroFrontendProps {
+  locale: string,
+  broadcastPayload: any,
+}
+
+const MicroFrontend: FunctionComponent<IMicroFrontendProps> = (
+  props: IMicroFrontendProps
+) => {
+  const { locale, broadcastPayload } = props;
+  const [ currentBroadcastPayload, setCurrentBroadcastPayload] = useState(broadcastPayload);
+
+  const localise = (localizationKey: string): string => {
+    return global?.IressTraderPlus?.UICore?.CultureInfo?.localize ? global.IressTraderPlus.UICore.CultureInfo.localize(localizationKey): localizationKey;
+  };
+
+  const handleBroadcastPaylodChange = (event: React.SyntheticEvent): void => {
+    setCurrentBroadcastPayload(JSON.parse(event.target.value));
+  }
+
+  const handleSubmit = (event: React.SyntheticEvent): void => {
+    event.preventDefault();
+    console.log("we need to do something here...", currentBroadcastPayload);
+  }
 
   return (
-    <Styles>
-      <div>
-        <Header widgetName={widgetName} />
-      </div>
-      <Table columns={columns} data={data} />
-    </Styles>
+    <>
+      <p>Broadcast payload: {JSON.stringify(broadcastPayload)}</p>
+      <p>Locale in micro frontend: <strong>{locale}</strong></p>
+      <p>
+        Translate in micro frontend "common.control.ok": <strong>{localise("common.control.ok")}</strong>
+      </p>
+      <form onSubmit={handleSubmit}>
+        <label>Broadcast Payload:</label>
+        <br />
+        <textarea 
+          value={JSON.stringify(currentBroadcastPayload)} 
+          onChange={handleBroadcastPaylodChange} 
+          style={
+            {
+              width: "100%",
+              height: "10%"
+            }
+          } 
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </>
   );
 }
 
