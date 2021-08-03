62461dae6cd037747ed373617ffed798d5c88b4e
Tatiana Ten <tatiana.ten@iress.com>
2021-08-03 14:52:45 +1000

    security info table replaced with a definition list

--- src/MicroFrontend.tsx
+++ src/MicroFrontend.tsx
@@ -1,25 +1,33 @@
-import React, { useState, useEffect, FunctionComponent } from 'react';
+import React, { useState, useEffect, FunctionComponent } from "react";
 import { sendBroadcast } from "./services/Broadcast";
-import { requestSecurityValidation, securityInformationGet } from "./services/ViewPointServices";
-import SecurityCodePicker from './components/SecurityCodePicker';
-import SecurityInformation from './components/SecurityInformation';
+import {
+  requestSecurityValidation,
+  securityInformationGet,
+} from "./services/ViewPointServices";
+import SecurityCodePicker from "./components/SecurityCodePicker";
+import SecurityInformation from "./components/SecurityInformation";
 
 export interface IMicroFrontendProps {
-  locale: string,
-  broadcastPayload: any,
-  elemSelector: string,
+  locale: string;
+  broadcastPayload: any;
+  elemSelector: string;
 }
 
 const MicroFrontend: FunctionComponent<IMicroFrontendProps> = (
   props: IMicroFrontendProps
 ) => {
   const { broadcastPayload, elemSelector } = props;
-  const [ currentBroadcastPayload, setCurrentBroadcastPayload] = useState(broadcastPayload);
-  
+  const [currentBroadcastPayload, setCurrentBroadcastPayload] =
+    useState(broadcastPayload);
+
   // just use the first security for the time being as our POC
   const currentSecurity = () => {
     let currentSecurity: string = "";
-    if(currentBroadcastPayload && currentBroadcastPayload.securities && currentBroadcastPayload.securities.length > 0) {
+    if (
+      currentBroadcastPayload &&
+      currentBroadcastPayload.securities &&
+      currentBroadcastPayload.securities.length > 0
+    ) {
       currentSecurity = currentBroadcastPayload.securities[0];
     }
     return currentSecurity;
@@ -27,29 +35,33 @@ const MicroFrontend: FunctionComponent<IMicroFrontendProps> = (
 
   const broadcastSecurity = () => {
     let currentSecurity: string = "";
-    if(broadcastPayload && broadcastPayload.securities && broadcastPayload.securities.length > 0) {
+    if (
+      broadcastPayload &&
+      broadcastPayload.securities &&
+      broadcastPayload.securities.length > 0
+    ) {
       currentSecurity = broadcastPayload.securities[0];
     }
     return currentSecurity;
   };
 
-  const [ security, setSecurity ] = useState(currentSecurity());
-  const [ securityInformation, setSecurityInformation ] = useState([]);
+  const [security, setSecurity] = useState(currentSecurity());
+  const [securityInformation, setSecurityInformation] = useState([]);
 
   const handleSecurityChange = async (securityCode: string) => {
-    if(await requestSecurityValidation(securityCode)) {
-      const securities = [
-        securityCode
-      ];
+    if (await requestSecurityValidation(securityCode)) {
+      const securities = [securityCode];
       const updatedBroadcastPayload = currentBroadcastPayload;
       updatedBroadcastPayload.securities = securities;
-  
+
       // update security
       setSecurity(securityCode);
-  
+
       // load the security information
-      setSecurityInformation(await securityInformationGet(elemSelector, securityCode));
-      
+      setSecurityInformation(
+        await securityInformationGet(elemSelector, securityCode)
+      );
+
       // update the broadcastPayload
       setCurrentBroadcastPayload(updatedBroadcastPayload);
 
@@ -61,15 +73,16 @@ const MicroFrontend: FunctionComponent<IMicroFrontendProps> = (
   const handleBroadcastChanged = async () => {
     const security = broadcastSecurity();
 
-    if(await requestSecurityValidation(security)) {
-    
+    if (await requestSecurityValidation(security)) {
       // update the security code
       setSecurity(security);
 
       // load the security information
-      setSecurityInformation(await securityInformationGet(elemSelector, security));
+      setSecurityInformation(
+        await securityInformationGet(elemSelector, security)
+      );
     }
-  }; 
+  };
 
   useEffect(() => {
     // if the broadcast payload has changed update the security value
@@ -78,13 +91,18 @@ const MicroFrontend: FunctionComponent<IMicroFrontendProps> = (
 
   return (
     <>
-      
-      <SecurityCodePicker security={security} handleOnChange={handleSecurityChange}></SecurityCodePicker>
-      { securityInformation && securityInformation.length > 0 && (
-        <SecurityInformation className="micro-frontend__table" securityInformation={securityInformation} />
+      <SecurityCodePicker
+        security={security}
+        handleOnChange={handleSecurityChange}
+      ></SecurityCodePicker>
+      {securityInformation && securityInformation.length > 0 && (
+        <SecurityInformation
+          className="micro-frontend__list-container"
+          securityInformation={securityInformation}
+        />
       )}
     </>
   );
-}
+};
 
 export default MicroFrontend;
--- src/components/SecurityInformation.tsx
+++ src/components/SecurityInformation.tsx
@@ -1,138 +1,136 @@
-import React, { FunctionComponent } from 'react';
-import { localise } from '../services/Localise';
+import React, { FunctionComponent } from "react";
+import { localise } from "../services/Localise";
 
 export interface ISecurityInformationProps {
-    securityInformation: any;
-    className: string;
+  securityInformation: any;
+  className: string;
 }
 
 const SecurityInformation: FunctionComponent<ISecurityInformationProps> = (
   props: ISecurityInformationProps
 ) => {
-    const { securityInformation, className } = props;
+  const { securityInformation, className } = props;
 
-    const localisationKey = (key: string) => `column.${key}`;
-    
-    // TODO: there's a bunch of rows not rendered - figure out which ones to exclude see SecurityInfoGridRows.ts
-    const  {
-        securityTypeCode,
-        firstListedDate,
-        quotedSharesOnIssue,
-        marketCapitalisation,
-        earningsPerShare,
-        yearlyDividend,
-        priceToEarnings,
-        priceToAsset,
-        currencyDenomination,
-        currencyCode,
-        isin,
-        lastListedDate,
-        suspensionDate,
-        liquidityFactor,
-        gicsCode,
-        gicsSector,
-        gicsIndustryGroup,
-        gicsIndustry,
-        gicsSubIndustry,
-        lotSize,
-    } = securityInformation[0];
-    const securityYeild = securityInformation[0].yeild;
-    return (
-        <div className={className}>
-            <table>
-                <tbody>
-                <tr>
-                    <th>{localise(localisationKey('securityTypeCode'))}</th>
-                    <td>{securityTypeCode}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('firstListedDate'))}</th>
-                    <td>{firstListedDate}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('quotedSharesOnIssue'))}</th>
-                    <td>{quotedSharesOnIssue}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('marketCapitalisation'))}</th>
-                    <td>{marketCapitalisation}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('earningsPerShare'))}</th>
-                    <td>{earningsPerShare}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('yearlyDividend'))}</th>
-                    <td>{yearlyDividend}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('priceToEarnings'))}</th>
-                    <td>{priceToEarnings}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('priceToAsset'))}</th>
-                    <td>{priceToAsset}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('yield'))}</th>
-                    <td>{securityYeild}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('currencyDenomination'))}</th>
-                    <td>{currencyDenomination}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('currencyCode'))}</th>
-                    <td>{currencyCode}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('isin'))}</th>
-                    <td>{isin}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('lastListedDate'))}</th>
-                    <td>{lastListedDate}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('lastListedDate'))}</th>
-                    <td>{lastListedDate ? lastListedDate : '-'}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('suspensionDate'))}</th>
-                    <td>{suspensionDate? suspensionDate : '-'}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('liquidityFactor'))}</th>
-                    <td>{liquidityFactor}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('gicsCode'))}</th>
-                    <td>{gicsCode}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('gicsSector'))}</th>
-                    <td>{gicsSector}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('gicsIndustryGroup'))}</th>
-                    <td>{gicsIndustryGroup}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('gicsIndustry'))}</th>
-                    <td>{gicsIndustry}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('gicsSubIndustry'))}</th>
-                    <td>{gicsSubIndustry}</td>
-                </tr>
-                <tr>
-                    <th>{localise(localisationKey('lotSize'))}</th>
-                    <td>{lotSize ? lotSize : '-'}</td>
-                </tr>
-                </tbody>
-            </table>
+  const localisationKey = (key: string) => `column.${key}`;
+
+  // TODO: there's a bunch of rows not rendered - figure out which ones to exclude see SecurityInfoGridRows.ts
+  const {
+    securityTypeCode,
+    firstListedDate,
+    quotedSharesOnIssue,
+    marketCapitalisation,
+    earningsPerShare,
+    yearlyDividend,
+    priceToEarnings,
+    priceToAsset,
+    currencyDenomination,
+    currencyCode,
+    isin,
+    lastListedDate,
+    suspensionDate,
+    liquidityFactor,
+    gicsCode,
+    gicsSector,
+    gicsIndustryGroup,
+    gicsIndustry,
+    gicsSubIndustry,
+    lotSize,
+  } = securityInformation[0];
+  const securityYeild = securityInformation[0].yeild;
+  return (
+    <div className={className}>
+      <dl>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("securityTypeCode"))}</dt>
+          <dd>{securityTypeCode}</dd>
         </div>
-    );
-}
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("firstListedDate"))}</dt>
+          <dd>{firstListedDate}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("quotedSharesOnIssue"))}</dt>
+          <dd>{quotedSharesOnIssue}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("marketCapitalisation"))}</dt>
+          <dd>{marketCapitalisation}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("earningsPerShare"))}</dt>
+          <dd>{earningsPerShare}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("yearlyDividend"))}</dt>
+          <dd>{yearlyDividend}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("priceToEarnings"))}</dt>
+          <dd>{priceToEarnings}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("priceToAsset"))}</dt>
+          <dd>{priceToAsset}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("yield"))}</dt>
+          <dd>{securityYeild}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("currencyDenomination"))}</dt>
+          <dd>{currencyDenomination}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("currencyCode"))}</dt>
+          <dd>{currencyCode}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("isin"))}</dt>
+          <dd>{isin}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("lastListedDate"))}</dt>
+          <dd>{lastListedDate}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("lastListedDate"))}</dt>
+          <dd>{lastListedDate ? lastListedDate : "-"}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("suspensionDate"))}</dt>
+          <dd>{suspensionDate ? suspensionDate : "-"}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("liquidityFactor"))}</dt>
+          <dd>{liquidityFactor}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("gicsCode"))}</dt>
+          <dd>{gicsCode}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("gicsSector"))}</dt>
+          <dd>{gicsSector}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("gicsIndustryGroup"))}</dt>
+          <dd>{gicsIndustryGroup}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("gicsIndustry"))}</dt>
+          <dd>{gicsIndustry}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("gicsSubIndustry"))}</dt>
+          <dd>{gicsSubIndustry}</dd>
+        </div>
+        <div className="data-list-group">
+          <dt>{localise(localisationKey("lotSize"))}</dt>
+          <dd>{lotSize ? lotSize : "-"}</dd>
+        </div>
+      </dl>
+    </div>
+  );
+};
 
 export default SecurityInformation;
